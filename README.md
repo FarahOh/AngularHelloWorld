27c5505b16f3b866d93bd0be7a4aec436dfc39f6
Farah <farahobaidullah@gmail.com>
2018-06-10 19:13:36 -0400

    Update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# HelloWorld
+# Heroku Angular Starter 
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to HelloWorld!');
+    expect(page.getParagraphText()).toEqual('Welcome to Heroku Angular Starter!');
   });
 });
