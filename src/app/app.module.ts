cb95fb80f5680d46186c2946420127ff373d2a43
Mikey <michael@kobetitsch.com>
2018-08-20 21:33:03 -0400

    encryption

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EncryptionComponent } from './encryption/encryption.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncryptionComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
